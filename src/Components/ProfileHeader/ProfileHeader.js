0b2570e0b93d42a5da5d9e4b9400c47ff3af8a6e
Pedro Bertao <pedrobertao11@hotmail.com>
2018-04-01 22:13:05 -0300

    Mudando a profile picture

--- src/Components/ProfileHeader/ProfileHeader.js
+++ src/Components/ProfileHeader/ProfileHeader.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Item, Icon } from 'semantic-ui-react'
 
+const profileImg = require('../../assets/profile.png');
 const profileHeader = (props) => {
-    let imageSize = window.innerWidth < 500 ? 'tiny' : 'medium';
+    let imageSize = !window.innerWidth < 500 ? 'medium' : 'mini';
     return (
         <Item.Group>
             <Item>
-                <Item.Image bordered circular size={imageSize} src='https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAAmlAAAAJGY5YzAwYjFiLWFmM2QtNGUxYi04OWZkLWRlZDVlYmExYmExOQ.jpg' />
+                <Item.Image circular avatar size={imageSize} src={profileImg}/>
                 <Item.Content verticalAlign='middle'>
                     <Item.Header>
                         Pedro Henrique Bertão Matos
