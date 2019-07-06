b166a1eee37cea576113f0e010d39a4fc4684ea9
Pedro Bertao <pedrobertao11@hotmail.com>
2019-07-05 22:01:25 -0300

    fixing on resize

--- src/Components/Navigation/Toolbar/Toolbar.js
+++ src/Components/Navigation/Toolbar/Toolbar.js
@@ -1,11 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Menu } from 'semantic-ui-react';
 
 import MenuMobile from './MenuMobile';
 import MenuDesktop from './MenuDesktop';
 
 export default (props) => {
-    let menu = window.innerWidth < 600 
+    const [isMobile, changeIsMobile] = useState(window.innerWidth < 600)
+    window.onresize = ()=> {
+        if (window.innerWidth < 600 && !isMobile)  {
+            changeIsMobile(true)
+        } else if(window.innerWidth >= 600 && isMobile) {
+            changeIsMobile(false)
+        }
+    }
+    const menu = isMobile
     ? <MenuMobile SideBarHandler={props.SideBarHandler} /> 
     : <MenuDesktop SideBarHandler={props.SideBarHandler} />
 
